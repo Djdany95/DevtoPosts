2532b35a1a7fcbc7bffc56c5c7c802af52430b1c
Daniel <djdany.djesus@gmail.com>
2018-09-14 16:28:39 +0200

    Disable feature
    Disabled /getallposts for heroku deploy.

--- app.py
+++ app.py
@@ -6,12 +6,12 @@ import dev_posts
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/getallposts', methods=['GET'])
-class getAllPosts(Resource):
-    def get(self, user):
-        json_data = dev_posts.getAllPosts(user)
-        data = json.loads(json_data)
-        return data
+# @app.route('/getallposts', methods=['GET'])
+# class getAllPosts(Resource):
+#     def get(self, user):
+#         json_data = dev_posts.getAllPosts(user)
+#         data = json.loads(json_data)
+#         return data
 
 @app.route('/getlastposts', methods=['GET'])
 class getLastPosts(Resource):
@@ -33,7 +33,7 @@ class getNTotalPosts(Resource):
         return data
 
 
-api.add_resource(getAllPosts, '/getallposts/<user>')
+# api.add_resource(getAllPosts, '/getallposts/<user>') --> Can't use it in heroku, but functions perfect locally.
 api.add_resource(getLastPosts, '/getlastposts/<user>')
 api.add_resource(checkLastPost, '/checklastpost/<user>')
 api.add_resource(getNTotalPosts, '/getntotalposts/<user>')
--- dev_posts.py
+++ dev_posts.py
@@ -1,8 +1,8 @@
-import time
 import urllib.request
 import json
-from platform import system
-from selenium import webdriver
+# import time
+# from platform import system
+# from selenium import webdriver
 from bs4 import BeautifulSoup
 
 
@@ -38,60 +38,60 @@ def getNTotalPosts(user):
     return total_posts
 
 
-def infiniteScroll(user):
-    """"""
-    user_url = url + '/' + user
+# def infiniteScroll(user):
+#     """"""
+#     user_url = url + '/' + user
 
-    options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    driver_path = './chromedriver.exe' if system() == 'Windows' else './chromedriver'
+#     options = webdriver.ChromeOptions()
+#     options.add_argument('--headless')
+#     driver_path = './chromedriver.exe' if system() == 'Windows' else './chromedriver'
 
-    browser = webdriver.Chrome(
-        executable_path=driver_path, chrome_options=options)
+#     browser = webdriver.Chrome(
+#         executable_path=driver_path, chrome_options=options)
 
-    # Tell Selenium to get the URL you're interested in.
-    browser.get(user_url)
+#     # Tell Selenium to get the URL you're interested in.
+#     browser.get(user_url)
 
-    # Selenium script to scroll to the bottom, wait 1 second for the next batch of data to load, then continue scrolling.  It will continue to do this until the page stops loading new data.
-    lenOfPage = browser.execute_script(
-        "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-    match = False
-    while(match == False):
-        lastCount = lenOfPage
-        time.sleep(1)
-        lenOfPage = browser.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
-        if lastCount == lenOfPage:
-            match = True
+#     # Selenium script to scroll to the bottom, wait 1 second for the next batch of data to load, then continue scrolling.  It will continue to do this until the page stops loading new data.
+#     lenOfPage = browser.execute_script(
+#         "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+#     match = False
+#     while(match == False):
+#         lastCount = lenOfPage
+#         time.sleep(1)
+#         lenOfPage = browser.execute_script(
+#             "window.scrollTo(0, document.body.scrollHeight);var lenOfPage=document.body.scrollHeight;return lenOfPage;")
+#         if lastCount == lenOfPage:
+#             match = True
 
-    # Now that the page is fully scrolled, grab the source code.
-    source_data = browser.page_source
+#     # Now that the page is fully scrolled, grab the source code.
+#     source_data = browser.page_source
 
-    return source_data
+#     return source_data
 
 
-def getAllPosts(user):
-    """"""
-    web = infiniteScroll(user)
+# def getAllPosts(user):
+#     """"""
+#     web = infiniteScroll(user)
 
-    soup = BeautifulSoup(web, 'html.parser')
+#     soup = BeautifulSoup(web, 'html.parser')
 
-    find_posts = soup.find_all('div', attrs={'class': 'single-article'})
+#     find_posts = soup.find_all('div', attrs={'class': 'single-article'})
 
-    posts_h3 = filter(lambda x: x is not None,
-                      map(lambda x: x.find('h3'), find_posts))
-    posts_names = list(map(
-        lambda x: x.text.strip(), posts_h3))
+#     posts_h3 = filter(lambda x: x is not None,
+#                       map(lambda x: x.find('h3'), find_posts))
+#     posts_names = list(map(
+#         lambda x: x.text.strip(), posts_h3))
 
-    posts_a = filter(lambda x: x is not None, map(
-        lambda x: x.find('a', attrs={'class': 'index-article-link'}), find_posts))
-    posts_urls = list(map(lambda x: url+x['href'], posts_a))
+#     posts_a = filter(lambda x: x is not None, map(
+#         lambda x: x.find('a', attrs={'class': 'index-article-link'}), find_posts))
+#     posts_urls = list(map(lambda x: url+x['href'], posts_a))
 
-    nPosts = list(range(len(posts_names), 0, -1))
+#     nPosts = list(range(len(posts_names), 0, -1))
 
-    response = create_json(nPosts, posts_names, posts_urls)
+#     response = create_json(nPosts, posts_names, posts_urls)
 
-    return response
+#     return response
 
 
 def getLastPosts(user):
